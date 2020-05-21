777c3d12a9b36d79aa1945c1979693294b2f1d98
team-pct <teampct@tuta.io>
2020-05-21 21:48:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const leet            = require('xtorrent');
+const rbg             = require('torrentapi-wrapper2');
+const tpb             = require('thepiratebay');
+module.exports = {
+  leet: leet,
+  rbg: rbg,
+  tpb: tpb
+};
